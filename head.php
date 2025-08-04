9243a0f149a09cf97609841f4eba21ad79d143b1
Dinosuar049 <nickeleevandenberg@gmail.com>
2025-08-04 18:43:19 +1200

    MAIN PAGE

---
+++ head.php
@@ -0,0 +1,40 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta name="description" content="Database of Untranslatable words - 
+    words that can't be translated into English using a single English
+    word">
+    <meta name="keywords" content="language, untranslatable, sentiment,
+    meaning, example, usage">
+
+    <title>Untranslatables</title>
+
+    <!--Link to make Font Awesome work -->
+    <script src="https://kit.fontawesome.com/533af2cdae.js" 
+    crossorigin="anonymous"></script>
+
+    <!-- Font Imports -->
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+
+    <!-- Links to style sheets fo here -->
+    <link rel="stylesheet" href="css/untrans_style.css">  <!-- grid areas -->
+    <link rel="stylesheet" href="css/untrans_nav.css">  <!--- navigation / buttons -->
+    <link rel="stylesheet" href="css/untrans_forms.css"> <!-- form styling -->
+    <link rel="stylesheet" href="css/flipcard.css"> <!-- flip card styling -->
+    <link rel="stylesheet" href="css/autocomplete.css"> <!-- autocomplete css -->
+
+    <!--grid areas -->
+    <link rel="stylesheet" href="css/untrans_nav.css">
+    <!-- navigation / buttons -->
+    
+    <!-- Links to javascrpts - use defer to prvent javascript trying to load before
+    have been created -->
+    <script src="js/side_bar.js" defer></script>
+
+</head>
